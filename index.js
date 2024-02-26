81a360a9f36e1daf683c7c0af327e2c7102edfaa
WickerPrison <cobylax17@gmail.com>
2024-02-26 12:51:00 -0700

    got basics set up

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "Write a short description of the project"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let output = `# ${data.title}
+    
+## Description
+${data.description}`;
+
+    fs.writeFile(fileName, output, (err) =>
+    err ? console.error(err) : console.log("Success"));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: questions[0],
+                name: "title"
+            },
+            {
+                type: "input",
+                message: questions[1],
+                name: "description"
+            }
+        ])
+        .then((response) => {
+            writeToFile("output.md", response);
+        });
+}
 
 // Function call to initialize app
 init();
